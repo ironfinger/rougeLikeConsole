d4cd713f55d1cf1f7f2e1fba1ce7fcfad1f6a8f6
ironfinger <tomhoughton888@gmail.com>
2016-12-19 21:50:51 +0000

    I got the progam to generate the grid exactly the same
    This had to be done because we don't want the grid to carry on
    regenerating.

--- rougeLike/rougeLike/Module1.vb
+++ rougeLike/rougeLike/Module1.vb
@@ -22,7 +22,7 @@
 
         procedurealGenerationNumbers()
         displayTerrian()
-
+        reDrawGrid()
         Console.ReadLine()
     End Sub
     Sub procedurealGenerationNumbers()
@@ -35,11 +35,11 @@
         landscapeSizeX = randomNumber.Next(0, 20)
         ReDim gridArray(landscapeSizeY, landscapeSizeX)
 
-        colour01 = randomNumber.Next(0, 6) 'This genereates a number to reference a colour for the colour pallete. 
-        colour02 = randomNumber.Next(0, 6)
-        colour03 = randomNumber.Next(0, 6)
-        colour04 = randomNumber.Next(0, 6)
-        colour05 = randomNumber.Next(0, 6)
+        colour01 = randomNumber.Next(1, 6) 'This genereates a number to reference a colour for the colour pallete. 
+        colour02 = randomNumber.Next(1, 6)
+        colour03 = randomNumber.Next(1, 6)
+        colour04 = randomNumber.Next(1, 6)
+        colour05 = randomNumber.Next(1, 6)
 
         If colour01 = colour02 Or colour01 = colour03 Then 'Just making sure no two variables are the same. 
             colour01 = randomNumber.Next(0, 6)
@@ -156,6 +156,98 @@
         displayNumberArray()
     End Sub
     Sub displayNumberArray()
+        For y = 0 To landscapeSizeY
+            For x = 0 To landscapeSizeX
+                Console.ForegroundColor = ConsoleColor.DarkGray
+                Console.Write("[" & gridArray(y, x) & "]")
+            Next
+            Console.WriteLine()
+        Next
+    End Sub
+    Sub reDrawGrid()
+        For y = 0 To landscapeSizeY
+            For x = 0 To landscapeSizeX
+                If gridArray(y, x) = 1 Then
+                    If colour01 = 1 Then
+                        Console.ForegroundColor = ConsoleColor.Blue
+                    ElseIf colour01 = 2 Then
+                        Console.ForegroundColor = ConsoleColor.Green
+                    ElseIf colour01 = 3 Then
+                        Console.ForegroundColor = ConsoleColor.Red
+                    ElseIf colour01 = 4 Then
+                        Console.ForegroundColor = ConsoleColor.Yellow
+                    ElseIf colour01 = 5 Then
+                        Console.ForegroundColor = ConsoleColor.White
+                    ElseIf colour01 = 6 Then
+                        Console.ForegroundColor = ConsoleColor.Magenta
+                    End If
+                ElseIf gridArray(y, x) = 2 Then
+                    If colour02 = 1 Then
+                        Console.ForegroundColor = ConsoleColor.Blue
+                    ElseIf colour02 = 2 Then
+                        Console.ForegroundColor = ConsoleColor.Green
+                    ElseIf colour02 = 3 Then
+                        Console.ForegroundColor = ConsoleColor.Red
+                    ElseIf colour02 = 4 Then
+                        Console.ForegroundColor = ConsoleColor.Yellow
+                    ElseIf colour02 = 5 Then
+                        Console.ForegroundColor = ConsoleColor.White
+                    ElseIf colour02 = 6 Then
+                        Console.ForegroundColor = ConsoleColor.Magenta
+                    End If
+                ElseIf gridArray(y, x) = 3 Then
+                    If colour03 = 1 Then
+                        Console.ForegroundColor = ConsoleColor.Blue
+                    ElseIf colour03 = 2 Then
+                        Console.ForegroundColor = ConsoleColor.Green
+                    ElseIf colour03 = 3 Then
+                        Console.ForegroundColor = ConsoleColor.Red
+                    ElseIf colour03 = 4 Then
+                        Console.ForegroundColor = ConsoleColor.Yellow
+                    ElseIf colour03 = 5 Then
+                        Console.ForegroundColor = ConsoleColor.White
+                    ElseIf colour03 = 6 Then
+                        Console.ForegroundColor = ConsoleColor.Magenta
+                    End If
+                ElseIf gridArray(y, x) = 4 Then
+                    If colour04 = 1 Then
+                        Console.ForegroundColor = ConsoleColor.Blue
+                    ElseIf colour04 = 2 Then
+                        Console.ForegroundColor = ConsoleColor.Green
+                    ElseIf colour04 = 3 Then
+                        Console.ForegroundColor = ConsoleColor.Red
+                    ElseIf colour04 = 4 Then
+                        Console.ForegroundColor = ConsoleColor.Yellow
+                    ElseIf colour04 = 5 Then
+                        Console.ForegroundColor = ConsoleColor.White
+                    ElseIf colour04 = 6 Then
+                        Console.ForegroundColor = ConsoleColor.Magenta
+                    End If
+                ElseIf gridArray(y, x) = 5 Then
+                    If colour05 = 1 Then
+                        Console.ForegroundColor = ConsoleColor.Blue
+                    ElseIf colour05 = 2 Then
+                        Console.ForegroundColor = ConsoleColor.Green
+                    ElseIf colour05 = 3 Then
+                        Console.ForegroundColor = ConsoleColor.Red
+                    ElseIf colour05 = 4 Then
+                        Console.ForegroundColor = ConsoleColor.Yellow
+                    ElseIf colour05 = 5 Then
+                        Console.ForegroundColor = ConsoleColor.White
+                    ElseIf colour05 = 6 Then
+                        Console.ForegroundColor = ConsoleColor.Magenta
+                    End If
+                ElseIf gridArray(y, x) = 6 Then
+
+                End If
+                Console.Write("[ ]")
+                System.Threading.Thread.Sleep(1)
+            Next
+            Console.WriteLine()
+        Next
+        Console.WriteLine()
+        Console.WriteLine("Numbers")
+        Console.WriteLine()
         For y = 0 To landscapeSizeY
             For x = 0 To landscapeSizeX
                 Console.ForegroundColor = ConsoleColor.DarkGray
